c50fbe58e542bd20d50b6f7d79501e78668bc052
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-07 18:24:41 -0400

    add user to the database using exported function

--- routes/index.ts
+++ routes/index.ts
@@ -4,15 +4,12 @@ module.exports = db => {
   const dbHelpers = require("./dbHelpers/dbHelpers.ts")(db)
 
   router.get("/", (req, res) => {
-    dbHelpers
-      .getUserWithEmail("alice@hotmail.com")
-      .then(data => res.send(data))
+    dbHelpers.getUserWithEmail("alice@hotmail.com").then(data => res.send(data))
   })
 
   router.post("/register", (req, res) => {
-    const firstName = req.body
-    console.log('Got body:', firstName);
-    res.send("ok")
+    const user = req.body
+    dbHelpers.addUser(user).then(data => console.log(data))
   })
   return router
 }
