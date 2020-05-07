5bbb81d6420f71f48bd9689785a62c1163636c33
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-05-06 23:06:32 -0400

    add db setup script

--- db/seeds/04_posts.sql
+++ db/seeds/04_posts.sql
@@ -1,14 +1,14 @@
 INSERT INTO posts
   (group_id, user_id, data, created_at)
 VALUES
-  (1, 1, { "test":"test post in group 1" }, NOW());
+  (1, 1, '{ "test":"test post in group 1" }', NOW());
 
 INSERT INTO posts
   (group_id, user_id, data, created_at)
 VALUES
-  (2, 3, { "test":"test post in group 3" }, NOW());
+  (2, 3, '{ "test":"test post in group 3" }', NOW());
 
 INSERT INTO posts
   (group_id, user_id, data, created_at)
 VALUES
-  (3, 6, { "test":"test post in group 3" }, NOW());
+  (3, 6, '{ "test":"test post in group 3" }', NOW());
