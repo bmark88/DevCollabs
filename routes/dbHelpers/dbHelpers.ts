30948308a1c24370945af579479388e6c8589901
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-07 15:42:26 -0400

    connect server to db

---
+++ routes/dbHelpers/dbHelpers.ts
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt")
+
+module.exports = db => {
+  /**
+   * Get a single user from the db given their email.
+   * @param {String} user  The email of the user.
+   * @return {Promise<{}>} A promise to the user.
+   */
+  const getUserWithEmail = function (email) {
+    return db
+      .query(
+        `
+        SELECT * FROM users
+        WHERE email=$1
+        LIMIT 1;
+        `,
+        [email]
+      )
+      .then(res => {
+        if (res.rows.length === 0) return null
+        return res.rows[0]
+      })
+  }
+
+  return {getUserWithEmail}
+}
