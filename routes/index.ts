a92507cb9fd86ea78f39be421061d1c82b346749
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-07 17:56:50 -0400

    make post route to register user

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,18 @@
 const router = require("express").Router()
 
 module.exports = db => {
-  const dbHelpers = require("./dbHelpers/dbHelpers.ts")(db);
+  const dbHelpers = require("./dbHelpers/dbHelpers.ts")(db)
 
   router.get("/", (req, res) => {
-      dbHelpers.getUserWithEmail("alice@hotmail.com").then((data)=> res.send(data))
+    dbHelpers
+      .getUserWithEmail("alice@hotmail.com")
+      .then(data => res.send(data))
   })
-  return router;
+
+  router.post("/register", (req, res) => {
+    const firstName = req.body
+    console.log('Got body:', firstName);
+    res.send("ok")
+  })
+  return router
 }
