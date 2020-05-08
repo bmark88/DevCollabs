4add56969390031716ae97e9d1389a95af7be6e5
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-08 13:26:14 -0400

    add login route

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,6 @@
 const router = require("express").Router()
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken');
 
 module.exports = db => {
   const dbHelpers = require("./dbHelpers/dbHelpers.ts")(db)
@@ -10,6 +12,25 @@ module.exports = db => {
   router.post("/register", (req, res) => {
     const user = req.body
     dbHelpers.addUser(user).then(data => console.log(data))
+  })
+
+  router.post("/login", (req, res) => {
+    const { username, password } = req.body
+    dbHelpers.checkForUser(username).then(user => {
+      if (user) {
+        if (bcrypt.compareSync(password, user.password)) {
+          jwt.sign({ user }, "secretkey", { expiresIn: "30s" }, (err, token) => {
+              const id = user.id
+              res.json({token , username, id })
+            })
+        } else {
+          res.status(400).send("Wrong Password")
+        }
+      } else {
+        res.status(400).send("User Not Found")
+      }
+    })
+    .catch(e => console.error(e))
   })
 
   return router
