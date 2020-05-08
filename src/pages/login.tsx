b04ab7f068e8979c1ae89574ac68e01f4cda5489
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-08 14:28:32 -0400

    create a simple form and a page for login

---
+++ src/pages/login.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+import LoginForm from '../components/LoginForm'
+import Layout from '../components/layout'
+
+
+const RegisterPage = () => (
+   <Layout>
+      <h1>Login</h1>
+      <LoginForm />
+   </Layout>
+)
+
+export default RegisterPage
