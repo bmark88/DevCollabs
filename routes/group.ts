5f35374299a1ae3228ab9ec814f57381077416ad
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-07 21:00:14 -0400

    made new route for group

---
+++ routes/group.ts
@@ -0,0 +1,24 @@
+const router = require("express").Router()
+
+module.exports = db => {
+  const dbHelpers = require("./dbHelpers/dbHelpers.ts")(db)
+
+  router.get("/:group_id", (req, res) => {
+    const { group_id } = req.params
+    dbHelpers.getPostWithGroupID(group_id).then(queryResults => {
+      console.log(queryResults)
+      const { username, data, created_at, avatar_image } = queryResults
+      console.log(username, data, created_at, avatar_image)
+      console.log(JSON.parse(data))
+      res.send({
+        username, 
+        data: JSON.parse(data),
+        created_at,
+        avatar_image
+      })
+
+    })
+  })
+
+  return router
+}
