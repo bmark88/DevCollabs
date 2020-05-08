50050eee478caa5a2e2548cf814d56e3072a9623
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-08 14:27:50 -0400

    removed unused import

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import styled from "styled-components"
 import RegisterForm from '../components/RegisterForm'
-import Navbar from '../components/Navbar'
 import Layout from '../components/layout'
 
 
