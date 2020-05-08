f9fd1c4daeb8896920bcbf08cde81039499dfccb
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-08 19:28:33 -0400

    Style registration form

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-import styled from "styled-components"
 import RegisterForm from '../components/RegisterForm'
 import Layout from '../components/layout'
 
 
 const RegisterPage = () => (
    <Layout>
-      <h1>Register</h1>
       <RegisterForm />
    </Layout>
 )
