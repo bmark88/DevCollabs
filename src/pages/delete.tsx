6b696e4a686e8502023966630b2bf14cf4c34cca
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-05-08 23:01:50 -0400

    add delete route

--- src/pages/delete.tsx
+++ src/pages/delete.tsx
@@ -4,13 +4,20 @@ import Users from "../components/users"
 import Groups from "../components/groups"
 import { Rooms, Room } from "../components/rooms"
 import Layout from "../components/layout"
+import axios from "axios"
 
 interface Props {
-    children: ReactNode
+  children: ReactNode
 }
+
 let data = JSON.parse(localStorage.getItem("session"))
-// data.data.id 
-console.log('data', data)
+
+axios({
+  method: "delete",
+  url: "http://localhost:8000/delete/1",
+  data: data.id 
+}) //TODO change from hard coded
+console.log("data", data)
 const deletePage = () => {
   return (
     <Layout>
