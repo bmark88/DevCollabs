64cabfffce85db2b686232999929ff1187f4941c
bmark88 <bradley.mark@hotmail.com>
2020-05-09 12:10:42 -0400

    Prevent user from being logged in if session does not exist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import styled from "styled-components"
 // import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
+import LoginForm from "../components/LoginForm"
+import Layout from "../components/layout"
 import Chat from "../components/chat"
 import { Topics, Topic, SubTopic } from "../components/topics"
 import Add from "../components/add"
@@ -17,6 +19,14 @@ const Main = styled.main`
 
 export default function IndexPage() {
   let { users, messages, handleSubmit } = App()
+  
+  if(!localStorage.getItem('session')) {
+    return (
+      <Layout>
+        <LoginForm />
+      </Layout>
+    )
+  }
 
   return (
     <>
