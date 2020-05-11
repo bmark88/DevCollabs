11f2af05d4c74f9b6d5414c4a72e72638e3f22a2
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-05-11 17:19:15 -0400

    gets posts for group

--- src/components/hooks/useApplicationData.tsx
+++ src/components/hooks/useApplicationData.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 export default function useApplicationData() {
-
-
   const [state, setState] = useState({
     group: 0,
     groups: [],
-    posts: {}
+    posts: {},
   })
 
   useEffect(() => {
@@ -15,18 +13,22 @@ export default function useApplicationData() {
   }, [])
 
   //get a users id from session json data. returns {id:interger}
-  const userId = JSON.parse(localStorage.getItem('session')).id
+  const userId = JSON.parse(localStorage.getItem("session")).id
 
   //gets all group names of a user. returns {array<[id:interger ,name:string]>} data
   const fetchGroups = () => {
     axios
       .get(`http://localhost:3001/group/u/${userId}`)
       .then(response => {
-        console.log(response.data)
-        setState({...state,
-          group: response.data[0],
-          groups: response.data,
-        })
+        return { group: response.data[0].id, groups: response.data }
+      })
+      .then(group => {
+        axios
+          .get(`http://localhost:3001/group/g/${group.group}`)
+          .then(response => {
+            setState({ ...state, posts: response.data, ...group })
+          })
+          .catch(error => console.log(error))
       })
       .catch(error => console.log(error))
   }
