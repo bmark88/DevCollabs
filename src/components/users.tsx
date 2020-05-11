f5ba1b753b51a42c99a7a439eb1c84ed8f3b1116
bmark88 <bradley.mark@hotmail.com>
2020-05-11 10:48:27 -0400

    Add unique keys to when mapping through components

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -51,8 +51,8 @@ const Users = ({ users, messages, handleSubmit }: Props) => {
     <Div>
       <UserList>
         <h1>Online Members</h1>
-        {users.map((user: string) => (
-          <h3>{user}</h3>
+        {users.map((user :string, index :number) => (
+          <h3 key={index}>{user}</h3>
         ))}
       </UserList>
       {messages.map((msg: any) => (
