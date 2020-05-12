58f2e5a71effe19d8ba52557c096030555c9b982
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-12 15:04:11 -0400

    change to use socketchat hook

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,36 +1,16 @@
 import React, { useState, useEffect } from "react"
 import { navigate } from "gatsby"
-import io from "socket.io-client"
 import Chat from "../components/Chat"
 import CodeSnippet from "../components/CodeSnippet"
 import Layout from "../components/layout"
-
+import socketChat from "../components/hooks/socketChat"
 const RoomPage = () => {
-  const [users, setUsers] = useState([])
-  const [user, setUser] = useState({})
-  const [messages, setMessages] = useState([])
-  const [connection, setConnection] = useState({})
+  let { users, messages, handleSubmit } = socketChat('Room 1')
 
   if (!localStorage.getItem("session")) {
     navigate("/login")
     return null
   }
-
-  useEffect(() => {
-    const { username } = JSON.parse(localStorage.getItem("session"))
-    const roomId = "Room1"
-    //server connection
-    const conn = io.connect("http://localhost:3001/room")
-
-    setConnection(conn)
-    //once connection is set send server your username and roomId
-    conn.emit("initial", {
-      username,
-      roomId,
-    })
-  }, [])
-
-  const handleSubmit = () => {}
 
   return (
     <Layout>
