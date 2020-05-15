d26f77804569a603575ffef5c57162028571f539
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-05-15 17:16:13 -0400

    shows results if they exists

--- src/components/GithubSearch.tsx
+++ src/components/GithubSearch.tsx
@@ -12,20 +12,37 @@ import AccountCircle from "@material-ui/icons/AccountCircle"
 import styled from "styled-components"
 import Button from "@material-ui/core/Button"
 
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}))
+
 export default function GithubSearch() {
   const [username, setUsername] = useState("")
-console.log(username)
-  const useStyles = makeStyles(theme => ({
-    margin: {
-      margin: theme.spacing(1),
-    },
-  }))
+  const [results, setResults] = useState({})
 
-  const getUserGithubApi = (event:any) => {
+  console.log(username)
+
+  const getUserGithubApi = (event: any) => {
     event.preventDefault()
-    axios.get(`https://api.github.com/search/users?q=${username}`).then(res => {
+    //if username is exact
+
+    axios.get(`https://api.github.com/users/${username}`).then(res => {
       console.log(res.data)
+      setResults(res.data)
     })
+
+    //does a general search
+    // axios.get(`https://api.github.com/search/users?q=${username}`).then(res => {
+    //   //if multiple users returned
+    //   if (res.data.items.length > 1) {
+    //   }
+
+    //   console.log(res.data)
+    //   console.log(res.data.items[0])
+    //   setResults(res.data.items[0])
+    // })
   }
 
   const classes = useStyles()
@@ -41,13 +58,48 @@ console.log(username)
             <TextField
               id="github-search-username"
               label="Enter username"
-              id="username"
-              label="Username"
               value={username}
               onChange={event => setUsername(event.target.value)}
             />
           </Grid>
         </Grid>
+        <Button
+          type="submit"
+          value="Submit"
+          onSubmit={getUserGithubApi}
+          variant="outlined"
+        >
+          Search
+        </Button>
+      </form>
+      {results.name && <div> Name: {results.name} </div>}
+      {results.company && <div> Company: {results.company} </div>}
+      {results.location && <div> Location: {results.location} </div>}
+      {results.email && <div> Email: {results.email} </div>}
+      {results.hireable && <div> Name: {results.hireable} </div>}
+      {results.bio && <div> Bio: {results.bio} </div>}
+      {results.html_url && <div> URL: {results.html_url} </div>}
+      {results.avatar_url && <div> Avatar: {results.avatar_url} </div>}
+      {results.followers_url && <div> Followers: {results.followers} </div>}
+      {results.following_url && <div> Following: {results.following} </div>}
+      {results.organizations_url &&<div> Organizations: {results.organizations_url} </div>}
+      {results.public_repos && <div> Repos: {results.public_repos} </div>}
+      {results.repos_url && <div> Repos URL: {results.repos_url} </div>}
+
+      <div>Filters</div>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="# of repos"
+          variant="outlined"
+          size="small"
+        />
+        <TextField
+          id="outlined-basic"
+          label="# of followers"
+          variant="outlined"
+          size="small"
+        />
         <Button
           type="submit"
           value="Submit"
