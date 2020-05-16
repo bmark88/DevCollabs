01d4ce17fd5d33bf36e7b1930f5cb17ba12010de
bmark88 <bradley.mark@hotmail.com>
2020-05-16 13:30:01 -0400

    Setup profile page with UserProfile component

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import styled from "styled-components"
+
+const UserProfile = () => {
+  return (
+    <div> Hello World </div>
+  )
+};
+export default UserProfile;
---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout"
+import UserProfile from "../components/UserProfile"
+
+const ProfilePage = () => {
+  return (
+    <Layout>
+      <UserProfile/>
+    </Layout>
+  )
+}
+
+export default ProfilePage;
