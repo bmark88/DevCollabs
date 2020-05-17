69c7cf8fca4b672772c7e367a1ce77128f581274
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-17 18:19:31 -0400

    use Rooms to make cards for profile and user list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,36 +1,48 @@
 import React from "react";
 import styled from "styled-components";
+import {Card, Typography} from '@material-ui/core'
+import { RoomCard} from '../components/rooms'
+
 
 interface Props {
   users :any
 }
 
 const UserContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+  margin: 1em;
+  width: 100%;
 `;
 
 const Ul = styled.ul`
   list-style: none;
+  height: 400px;
+  overflow: hidden; 
+  overflow-y: scroll;
+  
+`;
+
+const Li = styled.li`
+  display: inline
 `;
 
 const UserList = ({ users } :Props) => {
   // console.log(users.map(user => user))
   return (
-    <UserContainer>
-      <h1>Users List</h1>
+    <RoomCard title= "Users List" image="https://bit.ly/3bDrCnh">
+      <UserContainer >
       <Ul>
         {users.map((user :any, index :number) => {
           return (
             <div key={index}>
-              <li>{user.username}</li>
+              <Li>{user.username}</Li>
               <button>Add rating</button>
               {/* <div>{user.email}</div> */}
             </div>
           )
         })}
       </Ul>
-    </UserContainer>
+      </UserContainer>
+    </RoomCard>
     )
 }
 
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,26 +1,33 @@
 import React from "react"
 import styled from "styled-components"
 import { List, ListSubheader } from "@material-ui/core"
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-
+import AccountCircleIcon from "@material-ui/icons/AccountCircle"
+import { makeStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+import { Divider } from "@material-ui/core"
+import { RoomCard} from '../components/rooms'
 interface Props {
-  postCount :number
-  subscriptions :any
-  userName :string
+  postCount: number
+  subscriptions: any
+  userName: string
 }
 
 const Div = styled.div`
   display: flex;
   align-items: center;
   margin: 1em;
-`;
+`
 
 const Content = styled.div`
   border: solid;
   margin: 1em;
-  min-width: 345px;
+  min-width: 10%;
   height: 400px;
-`;
+`
 
 const AdminBadge = styled.div`
   margin-left: 1em;
@@ -28,40 +35,39 @@ const AdminBadge = styled.div`
   border-radius: 20px;
   padding: 0.1em;
   border: solid 1px;
-`;
+`
 
 const ListTitle = styled.h3`
   background-color: white;
-`;
+`
 
-const UserProfile = ({ postCount, subscriptions, userName } :Props) => {
+const UserProfile = ({ postCount, subscriptions, userName }: Props) => {
   return (
-    <>
-    <Content>
-      <List style={{maxHeight: '100%', overflow: 'auto'}}>
-      <h1>{userName}</h1>
-      <h3>Total Posts: {postCount}</h3>
-      <ListSubheader>
-        <ListTitle>
-          {`Your Subbed Groups`}
-        </ListTitle>
-      </ListSubheader>
-        {subscriptions.map((group :any, index :number) => {
-          return (
-            <Div key={index}>
-              <b>{group.name}</b>
-              {group.is_admin && 
-                <AdminBadge>
-                  <AccountCircleIcon/>
-                  Admin
-                </AdminBadge>
-              }
-            </Div>
-          );
-        })}
-      </List>
-    </Content>
-    </>
-  );
-};
-export default UserProfile;
+    <RoomCard title= {userName} image="https://hackernoon.com/hn-images/1*TYAzzTJ60x-qg5N81ElU9A.png" >
+        <div>
+        <Typography variant="h6" gutterBottom>
+            Total Posts: {postCount}
+          </Typography>
+          <Divider />
+          <ListSubheader></ListSubheader>
+            <ListTitle>{`Your Subbed Groups`}</ListTitle>
+          
+          {subscriptions.map((group: any, index: number) => {
+            return (
+              <Div key={index}>
+                <b>{group.name}</b>
+                {group.is_admin && (
+                  <AdminBadge>
+                    <AccountCircleIcon />
+                    Admin
+                  </AdminBadge>
+                )}
+              </Div>
+            )
+          })}
+          </div>
+    </RoomCard>
+    
+  )
+}
+export default UserProfile
