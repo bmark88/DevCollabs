64c3db0e94e87e9497ef2303f5f5284994bfbb0e
bmark88 <bradley.mark@hotmail.com>
2020-05-17 14:42:47 -0400

    Add rating button

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,13 +1,37 @@
 import React from "react";
+import styled from "styled-components";
 
 interface Props {
   users :any
 }
 
+const UserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Ul = styled.ul`
+  list-style: none;
+`;
+
 const UserList = ({ users } :Props) => {
+  // console.log(users.map(user => user))
   return (
-    <h1>Users List</h1>
-  )
+    <UserContainer>
+      <h1>Users List</h1>
+      <Ul>
+        {users.map((user :any, index :number) => {
+          return (
+            <div key={index}>
+              <li>{user.username}</li>
+              <button>Add rating</button>
+              {/* <div>{user.email}</div> */}
+            </div>
+          )
+        })}
+      </Ul>
+    </UserContainer>
+    )
 }
 
 export default UserList;
