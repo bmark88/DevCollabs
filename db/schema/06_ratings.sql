0e5c8903b3b02bd299d0416e6677511a266bbd6d
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-17 21:06:34 -0400

    change rated user id to interger and added one rating to check avg

--- db/schema/06_ratings.sql
+++ db/schema/06_ratings.sql
@@ -3,6 +3,6 @@ CREATE TABLE ratings
 (
   id SERIAL PRIMARY KEY NOT NULL,
   rater_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  rated_user_id VARCHAR(255) NOT NULL,
+  rated_user_id INTEGER NOT NULL,
   rating SMALLINT NOT NULL DEFAULT 0
 );
