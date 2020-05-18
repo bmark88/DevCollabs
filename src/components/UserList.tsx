00becfe74c8ead0fdda4b9d639b9460624d1b239
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-05-17 21:08:51 -0400

    moved map elements to own component

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,59 +1,75 @@
-import React from "react";
-import styled from "styled-components";
-import {Card, Typography} from '@material-ui/core'
-import { RoomCard} from '../components/rooms'
+import React, { useState } from "react"
+import styled from "styled-components"
+import { Card, Typography } from "@material-ui/core"
+import { RoomCard } from "../components/rooms"
+import Rating from "@material-ui/lab/Rating"
+import { makeStyles } from '@material-ui/core/styles';
+import UserListItem from './userListItem'
 
+const useStyles = makeStyles({
+  rate: {
+    position: 'absolute',
+    right: '0px'
+  },
+});
 
 interface Props {
-  users :any
+  users: any
 }
 
 const UserContainer = styled.div`
   width: 100%;
-`;
+`
 
 const Ul = styled.ul`
   list-style: none;
   height: 400px;
-  overflow: hidden; 
+  overflow: hidden;
   overflow-y: scroll;
-  
-`;
+`
 
 const Li = styled.li`
-  display: inline
-`;
-
-const Button =styled.button`
-  position: absolute;
-  right: 0px
+  display: inline;
 `
+
 
 const Div = styled.div`
   position: relative;
   margin: 2px;
 `
 
+const UserList = ({ users }: Props) => {
+  const [value, setValue] = useState(0)
+  const classes = useStyles();
 
-const UserList = ({ users } :Props) => {
-  // console.log(users.map(user => user))
   return (
-    <RoomCard title= "Users List" image="https://bit.ly/3bDrCnh">
-      <UserContainer >
-      <Ul>
-        {users.map((user :any, index :number) => {
-          return (
-            <Div key={index} >
-              <Li>{user.username}</Li>
-              <Button>Add rating</Button>
-              {/* <div>{user.email}</div> */}
-            </Div>
-          )
-        })}
-      </Ul>
+    <RoomCard title="Users List" image="https://bit.ly/3bDrCnh">
+      <UserContainer>
+        <Ul>
+          {users.map((user: any, index: number) => {
+            return (
+              <UserListItem index= {index} user={user} />
+              // <Div key={index}>
+              //   <form onSubmit={handleOnSubmit}>
+              //     <Li>{user.username}</Li>
+              //     <Rating
+              //       className= {classes.rate}
+              //       name="simple-controlled"
+              //       value={0}
+              //       onChange={(event, newValue) => {
+              //       event.target.value = newValue
+              //       console.log(user)
+              //       console.log(newValue)
+              //     }}
+              //     />
+              //   </form>
+              // </Div>
+            )
+          })}
+        </Ul>
       </UserContainer>
     </RoomCard>
-    )
+  )
 }
 
-export default UserList;
+export default UserList
