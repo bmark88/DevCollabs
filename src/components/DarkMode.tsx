12a50258c32a27af0823c5501adc505a5ffa7852
bmark88 <bradley.mark@hotmail.com>
2020-05-19 16:15:39 -0400

    Clean up code

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
-import styled, { ThemeProvider, createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#EEE'};
     color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#111'}
   }
-
-  // div {
-  //   background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#EEE'};
-  //   color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#111'}
-  // }
-
-  // span {
-  //   color: ${props => props.theme.mode === 'dark' ? 'red' : 'ffb400'}
-  //   background-color: red;
-  // }
   
   .dark {
     background-color: ${props => props.theme.mode === 'dark' ? 'gray' : '#EEE'};
@@ -26,24 +15,6 @@ const GlobalStyle = createGlobalStyle`
     color: ${props => props.theme.mode === 'dark' ? 'red' : 'ffb400'}
     background-color: red;
   }
-
-  // .dark2 {
-  //   background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#EEE'};
-  //   color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#111'}
-  // }
-
-  p {
-    // background-color: gray;
-    // opacity: 70%;
-    // color: white;
-    // backgroundcolor: red;
-  }
-
-  // input {
-  //   background-color: white;
-  //   color: white;
-  // }
-  
 `;
 
 export default GlobalStyle
